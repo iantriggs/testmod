c38dc3fcdab02556f36b21be3c4eec3fbf166e5d
Ian Triggs <iantriggs@gmail.com>
2024-10-07 17:48:43 +1100

    Added docs

--- testmod.go
+++ testmod.go
@@ -1,5 +1,8 @@
+// This is a test package for Chapter 10 of Learning Go.
+// Not much more to say, really.
 package testmod
 
+// This function will add two integers together and return the result.
 func Add(a, b int) int {
 	return a + b
 }
